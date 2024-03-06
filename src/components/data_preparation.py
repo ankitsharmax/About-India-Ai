93bed061c5bc9fb37922670a8b5c2be58c581c44
ankitsharmax <kumarankitx022@gmail.com>
2024-03-06 15:30:07 +0530

    Bug fix in data loader

--- src/components/data_preparation.py
+++ src/components/data_preparation.py
@@ -5,29 +5,38 @@ from dataclasses import dataclass
 
 @dataclass
 class DataPreparationConfig:
-    data_path = os.path.join('data',"indian_cities.csv")
+    indian_cities = os.path.join('data',"indian_cities.csv")
+    data_scrapper_site = 'https://en.wikipedia.org/wiki/'
+    data_download_link = 'https://en.wikipedia.org/api/rest_v1/page/pdf/'
+    crawler_data = os.path.join('data','crawler_data.csv')
 
 class DataPreparation:
     def __init__(self):
-        self.ingestion_config = DataPreparationConfig()
+        self.data_config = DataPreparationConfig()
 
     def initiate_data_ingestion(self):
         try:
             df = pd.read_csv('data\worldcities.csv')
-
+            df = df[df["country"]=='India']
+            # df.to_csv(self.data_config.indian_cities,index=False,header=True)
+            df.reset_index(inplace=True)
             return df
         except:
             pass
 
-    def data_transformation(self,df):
-        try:
-            df = df[df["country"]=='India']
-            # return df
-            df.to_csv(self.ingestion_config.data_path,index=False,header=True)
-        except:
-            pass
+    def data_preprocessor(self,df):
+        df["page_url"] = None
+        df["download_url"] = None
+        df["download_status"] = None
+        print(df)
+        for i in range(df.shape[0]):
+            df.loc[i,"page_url"] = self.data_config.data_scrapper_site + df.loc[i,"city"]
+            df.loc[i,"download_url"] = self.data_config.data_download_link + df.loc[i,"city"]
+        # df.to_csv(self.data_config.crawler_data,index=False,header=True)
+        return df
+    # https://codereview.stackexchange.com/questions/160784/small-program-to-download-wikipedia-articles-to-pdf
 
 if __name__ == '__main__':
     data_obj = DataPreparation()
     df = data_obj.initiate_data_ingestion()
-    data_obj.data_transformation(df)
+    print(data_obj.data_preprocessor(df[["city"]]))
--- src/components/data_scrapper.py
+++ src/components/data_scrapper.py
@@ -1,13 +1,12 @@
 import os
 import sys
 import pandas as pd
+import pdfkit
 from dataclasses import dataclass
 
 class DataCrawlerConfig:
     data_path = 'data'
-    indian_cities = 'data\indian_cities.csv'
-    data_scrapper_site = 'https://en.wikipedia.org/wiki/'
-    crawler_data = os.path.join('data','crawler_data.csv')
+    crawler_data = 'data\crawler_data.csv'
 
 class DataCrawler:
     def __init__(self):
@@ -15,19 +14,14 @@ class DataCrawler:
 
     def data_loader(self):
         try:
-            df = pd.read_csv(self.data_configs.indian_cities)
-            return df[['city']]
+            df = pd.read_csv(self.data_configs.crawler_data)
         except:
             pass
     
-    def data_preprocessor(self,df):
-        df['url'] = None
-        for i in range(df.shape[0]):
-            df.loc[i,"url"] = self.data_configs.data_scrapper_site + df.loc[i,"city"]
-        df.to_csv(self.data_configs.crawler_data,index=False,header=True)
-        return df
+    def data_downloader(self,data):
+        pass
+        
     
 if __name__ == '__main__':
     data_obj = DataCrawler()
     df = data_obj.data_loader()
-    print(data_obj.data_preprocessor(df))
